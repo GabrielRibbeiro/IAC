0247ba016882191e2d9c1a01aa4adae50f961369
root <gabrielribbeirodematos@gmail.com>
2023-07-20 16:33:47 -0300

    Realizando o primeiro commit  da infraestrutura na  AWS

---
+++ iac.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-03f65b8614a860c29"
+  instance_type = "t2.micro"
+  key_name = "iac-student"
+
+  tags = {
+    Name = "iac_student"
+  }
+}
