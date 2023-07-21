1151457d67ead6663ad2974dc12ffdd03ca6dea7
root <gabrielribbeirodematos@gmail.com>
2023-07-20 23:49:41 -0300

    Criando nova  infraestutura

--- iac.tf
+++ iac.tf
@@ -16,12 +16,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
   key_name = "iac-student"
-  user_data = <<-EOF
-                #!/bin/bash
-                cd /home/ubuntu
-                echo "<h1> Feito com o terraform </h1>" > index.html
-                nohup busybox httpd -f -p 8080 &
-                 EOF
+  # user_data = <<-EOF
+             #   #!/bin/bash
+              #  cd /home/ubuntu
+               # echo "<h1> Feito com o terraform </h1>" > index.html
+               # nohup busybox httpd -f -p 8080 &
+                # https://github.com/EOF
   tags = {
     Name = "teste AWS"
   }
